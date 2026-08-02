7110e3db3c148de3e17c7c89a65951d8b259fdb7
xubmxd <shubhamrajsrivastava2006@gmail.com>
2026-08-02 22:36:12 +0530

    integrated seek feature

--- mewsic/bindings.py
+++ mewsic/bindings.py
@@ -8,6 +8,8 @@ Format: ("key", "action_name", "Description")
 MEWSIC_BINDINGS = [
     ("space", "toggle_playback", "Play/Pause"),
     ("n", "skip_track", "Skip to Next"),
+    ("right", "seek_forward", "Seek +10s"),
+    ("left", "seek_backward", "Seek -10s"),
     ("=", "volume_up", "Vol +"),
     ("-", "volume_down", "Vol -"),
     ("q", "quit", "Quit App")
